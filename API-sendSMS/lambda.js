786df6c06040e46b89250e13f397e5fef245d25b
omaarali <omaralidirie@yahoo.com>
2018-11-05 16:41:34 +0100

    values saved now in the lambda event for example the phone number etc

--- API-sendSMS/lambda.js
+++ API-sendSMS/lambda.js
@@ -2,25 +2,28 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
-    let receiver = event['receiver'];
-    let sender = event['sender'];
-    let message = event['message'];
+	let receiver = event['receiver'];
+	let sender = event['sender'];
+	let message = event['message'];
 
-    console.log("Sending message", message, "to receiver", receiver);
-    sns.publish({
-        Message: 'test',
-        MessageAttributes: {
-            'AWS.SNS.SMS.SMSType': {
-                DataType: 'String',
-                StringValue: 'Promotional'
-            },
-            'AWS.SNS.SMS.SenderID': {
-                DataType: 'String',
-                StringValue: 'fromSLAPPFORGE'
-            },
-        },
-        PhoneNumber: '+31616203400'
-  	}).promise()
+	let isPromotional = true;
+
+	console.log("Sending message", message, "to receiver", receiver);
+
+	sns.publish({
+		Message: message,
+		MessageAttributes: {
+			'AWS.SNS.SMS.SMSType': {
+				DataType: 'String',
+				StringValue: 'Promotional'
+			},
+			'AWS.SNS.SMS.SenderID': {
+				DataType: 'String',
+				StringValue: sender
+			},
+		},
+		PhoneNumber: receiver
+	}).promise()
 		.then(data => {
 			console.log("Sent message to", receiver);
 			callback(null, data);
